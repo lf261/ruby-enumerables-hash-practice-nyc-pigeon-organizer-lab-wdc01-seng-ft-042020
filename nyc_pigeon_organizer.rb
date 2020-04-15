74c4ff0a6c085669448fb1d1a2830c8f88cf27ac
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-15 16:05:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
-end
+
+pigeon_list = {}
+
+data.each do |first_level, second_level|
+  second_level.each do |data_type, names|
+    names.each do |name|
+      if !pigeon_list[name] 
+          pigeon_list[name] = {}
+      end 
+      if !pigeon_list[name][first_level] 
+          pigeon_list[name][first_level] = []
+      end
+      pigeon_list[name][first_level].push(data_type.to_s)
+  end
+  end
+end 
+
+
+ pigeon_list
+end 
